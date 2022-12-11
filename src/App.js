29abe6526a97756012bbdd67dbf11b4deaea6c22
Fede <federico@freightos.com>
2022-12-11 23:48:55 +0100

    create screen

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+const arrayKeys = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0",];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <span className="screen">Calling...</span>
+      <main className="telf">
+        <div className="keyboard" >
+           {arrayKeys.forEach(key => {
+            <button>{key}</button>
+           })}
+        </div>
+
+
+      </main>
     </div>
   );
 }
